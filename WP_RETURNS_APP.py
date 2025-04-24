3ddd396fba6986daf6321c2a9b91349f1ae25971
jakepeltiersmith2 <jake@sbinteriors.co.uk>
2025-04-24 14:26:56 +0100

    Update WP_RETURNS_APP.py

--- WP_RETURNS_APP.py
+++ WP_RETURNS_APP.py
@@ -49,7 +49,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # ——— CONFIG ———
-PAGE_SIZE        = 50
+PAGE_SIZE        = 100
 LOCAL_JSON       = os.path.join(os.path.dirname(__file__), "returns_posts.json")
 GITHUB_RAW_JSON  = "https://raw.githubusercontent.com/jakepeltiersmith2/WP-RETURNS-ARCHIVE/main/returns_posts.json"
 GITHUB_RAW_MEDIA = "https://raw.githubusercontent.com/jakepeltiersmith2/WP-RETURNS-ARCHIVE/main/media"
